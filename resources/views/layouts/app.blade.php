f097e7d6c43c4de0d6baa3913bbfa609ab172f79
HerrscherZiel <herrscherziel@gmail.com>
2019-07-24 10:18:08 +0700

    few Error Handle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,7 @@
           <div class="app-title">
             <h1><a href="{{URL::previous()}}"> <i class="fa fa-arrow-left mr-3"></i></a>{{ucwords(Session::get('title'))}}</h1>
         </div>
+
             @yield('content')
         </main>
     </div>
