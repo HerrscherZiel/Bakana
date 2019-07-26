a3b791e60f4973919b3de1ab22101465ee6c9e06
Kreautz <42375679+Kreautz@users.noreply.github.com>
2019-07-26 09:26:29 +0700

    timeline

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.user')
 
 @section('content')
+<div>
       <div class="tile row">
         <div class="col-md-12">
             <a href="/timelines/project" class="btn btn-primary">Show Project</a>
             <a href="/timelines/job" class="btn btn-primary">Show Job</a><br><br>
+
             <select class="form-control" id="project" name="project">
                 <option disabled="" selected="">Select Project</option>
                 @foreach($val as $vals)
@@ -21,7 +23,7 @@
     var events = [];
   $.get('/events/get', function(result){
       events = result;
-  });    
+  });
   $('#calendar').fullCalendar({
       events: function (start, end, timezone, callback) {
           callback(events);
