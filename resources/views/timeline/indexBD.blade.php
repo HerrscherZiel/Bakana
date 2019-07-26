7a7d66a443c003a83abf0d3c3d7ee6ca73ffa0a3
Kreautz <42375679+Kreautz@users.noreply.github.com>
2019-07-26 15:36:07 +0700

    timeline almost final ver

--- resources/views/timeline/indexBD.blade.php
+++ resources/views/timeline/indexBD.blade.php
@@ -20,56 +20,71 @@
             {!! $calendar->calendar() !!}
         </div>
       </div>
-<script type="text/javascript" src="{{URL::asset('docs/js/jquery-3.4.1.js')}}"></script>
+    <script src="{{URL::asset('js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{URL::asset('js/popper.min.js')}}"></script>
+    <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript">
+        var events = [];
+        $.get('/events/get', function(result){
+            events = result;
+        });
+        $('#calendar').fullCalendar({
+            events: function (start, end, timezone, callback) {
+                callback(events);
+            }
+        });
+    </script>
+    <!-- Calendar-->
+    <script type="text/javascript" src="{{URL::asset('js/plugins/jquery-ui.custom.min.js')}}"></script>
+    <script src="{{URL::asset('js/mhmoment.min.js')}}"></script>
+    <script src="{{URL::asset('js/mhfullcalendar.min.js')}}"></script>
 
-<script type="text/javascript">
-    var events = [];
-  $.get('/events/get', function(result){
-      events = result;
-  });
-  $('#calendar').fullCalendar({
-      events: function (start, end, timezone, callback) {
-          callback(events);
-      }
-  });
-</script>
-<!-- Calendar-->
-<script type="text/javascript" src="{{URL::asset('docs/js/plugins/jquery-ui.custom.min.js')}}"></script>
-{{--<script type="text/javascript" src="{{URL::asset('docs/js/plugins/moment.min.js')}}"></script>--}}
-{{--<script type="text/javascript" src="{{URL::asset('docs/js/plugins/fullcalendar.min.js')}}"></script>--}}
-<script src="{{URL::asset('docs/js/mhmoment.min.js')}}"></script>
-<script src="{{URL::asset('docs/js/mhfullcalendar.min.js')}}"></script>
-<script src="{{URL::asset('docs/js/main.js')}}"></script>
+    <script src="{{URL::asset('js/main.js')}}"></script>
 
-{{--<script type="text/javascript" src="{{URL::asset('docs/js/fullcalendar.min.js')}}"></script>--}}
 
-<!-- Option Ajax  -->
-{{--<script type="text/javascript">--}}
+    <!-- Option Ajax  -->
+    <script type="text/javascript">
 
-{{--    $(document).ready(function($){--}}
-{{--        $('#project').on('change', function() {--}}
-{{--            $.ajaxSetup({--}}
-{{--                headers: {--}}
-{{--                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
-{{--                }--}}
-{{--            });--}}
-{{--            if ($("#project").val() != "") {--}}
-{{--                $.ajax({--}}
-{{--                    type: "POST",--}}
-{{--                    url: '/timelines/' + $("#project option:selected").val(),--}}
-{{--                    data: { id: $("#project").val() },--}}
-{{--                    dataType: "json",--}}
-{{--                    success: function (data){--}}
-{{--                        console.log('success')--}}
-{{--                    },--}}
-{{--                    error: function (xhr) {--}}
-{{--                        alert("Something went wrong, please try again");--}}
-{{--                    }--}}
-{{--                })}--}}
-{{--        });--}}
-{{--    });--}}
+        $(document).ready(function($){
+            $('#project').on('change', function() {
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                if ($("#project").val() != "") {
+                    $.ajax({
+                        type: "POST",
+                        url: '/timelines/' + $("#project option:selected").val(),
+                        data: { id: $("#project").val() },
+                        dataType: "json",
+                        success: function (data){
+                            console.log('success')
+                            $('#calendar').fullCalendar( 'removeEvents' ,event );
+                            $('#calendar').fullCalendar(
+                                {"header":{
+                                        "left":"prev,next today",
+                                        "center":"title",
+                                        "right":"month,agendaWeek,agendaDay"
+                                    },
+                                    "eventLimit":true,
+                                    "events":[]});
 
-{{--</script>--}}
+                            {{--                        {!! $calendar->script() !!}--}}
+                            // $('#calendar').fullCalendar({
+                            //     events: function (start, end, timezone, callback) {
+                            //         callback(events);
+                            //     }
+                            // });
+                        },
+                        error: function (xhr) {
+                            alert("Something went wrong, please try again");
+                        }
+                    })}
+            });
+        });
+
+    </script>
 </div>
 {!! $calendar->script() !!}
 @endsection
